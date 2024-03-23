d7408b4eb9dfa65ae0c6a3e90857fec74f6d7b36
donnyscript <donnyscript@gmail.com>
2024-03-23 18:57:59 -0400

    Update error handling

--- commands/fun/ping.js
+++ commands/fun/ping.js
@@ -3,14 +3,14 @@ const ytsr = require("youtube-sr");
 const YouTube = require("youtube-sr").default;
 
 module.exports = {
-	category: 'utility',
+	category: 'fun',
 	data: new SlashCommandBuilder()
-		.setName('ping')
-		.setDescription('Replies with Pong!'),
+		.setName('snitch')
+		.setDescription('wanna know something?'),
 	async execute(interaction) {
 		const wait = (ms) => new Promise(resolve => setTimeout(resolve, ms));
 		let name = interaction.member.user.globalName;
-		await interaction.reply({content: `My name is ${name}, and I am officially coming out gay, thank you everyone.`, ephemeral: true, tts:true});
+		await interaction.reply({content: `${name} came out of the closet to me in private, thought everyone should know.`, ephemeral: true, tts:true});
 		await wait(15000);
 		await interaction.followUp({content: `${name} you though LOL`, ephemeral: true})
 	},
