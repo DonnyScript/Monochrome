cb871cd01800998231c811dc4900fd24e68beb9c
donnyscript <donnyscript@gmail.com>
2024-07-30 17:22:26 -0400

    Api Hit command

---
+++ commands/util/testRequest.js
@@ -0,0 +1,22 @@
+const { useQueue } = require("discord-player");
+const { SlashCommandBuilder } = require('@discordjs/builders');
+const wait = require('util').promisify(setTimeout);
+const axios = require('axios');
+
+
+module.exports = {
+    category: 'util',
+    data: new SlashCommandBuilder()
+        .setName('api_hit')
+        .setDescription('Hit the myAnimeList api'),
+    async execute(interaction) {
+        axios
+    .get('https://api.jikan.moe/v4/seasons/now')
+    .then((response) => {
+        console.log(response.data);
+    })
+    .catch((error) => {
+        console.log(error);
+    });
+    }
+};
