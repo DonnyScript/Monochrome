e3a6eb3226aafceea4ff7799f4f673f9d5a98cd4
donnyscript <donnyscript@gmail.com>
2024-07-30 15:55:14 -0400

    Autocomplete title search

--- commands/audio/play.js
+++ commands/audio/play.js
@@ -2,7 +2,7 @@ const { SlashCommandBuilder } = require('@discordjs/builders');
 const { useMainPlayer } = require('discord-player');
 const { EmbedBuilder } = require('discord.js');
 const wait = require('util').promisify(setTimeout);
-const excludedExtractors = [ 
+const excludedExtractors = [
     'VimeoExtractor',
     'SoundCloudExtractor',
     'ReverbnationExtractor',
@@ -12,6 +12,27 @@ const excludedExtractors = [
     'SpotifyExtractor',
 ];
 
+async function suggest(interaction) {
+    const query = interaction.options.getString('input', false)?.trim();
+    if (!query) return;
+
+    const player = useMainPlayer();
+
+    const searchResult = await player.search(query).catch(() => null);
+    if (!searchResult) {
+        return interaction.respond([{ name: 'No results found', value: '' }]);
+    }
+    const tracks = searchResult.hasPlaylist()
+        ? searchResult.playlist.tracks.slice(0, 24)
+        : searchResult.tracks.slice(0, 10);
+
+    const formattedResult = tracks.map((track) => ({
+        name: track.title.slice(0, 100),
+        value: track.url,
+    }));
+
+    await interaction.respond(formattedResult);
+}
 
 module.exports = {
     category: 'audio',
@@ -22,14 +43,20 @@ module.exports = {
             option
                 .setName('input')
                 .setDescription('Put YouTube video URL, video title, YouTube playlist here')
-                .setRequired(true)),
+                .setRequired(true)
+                .setAutocomplete(true)),
+    async autocomplete(interaction) {
+        await suggest(interaction);
+    },
     async execute(interaction) {
         await interaction.deferReply();
         const player = useMainPlayer();
         const channel = interaction.member.voice.channel;
         const query = interaction.options.getString('input', true);
 
-        if (!channel) return interaction.followUp('You are not connected to a voice channel!');
+        if (!channel) {
+            return interaction.followUp('You are not connected to a voice channel!');
+        }
 
         try {
             const { track } = await player.play(channel, query, {
@@ -37,19 +64,21 @@ module.exports = {
                     leaveOnEmpty: false,
                     leaveOnEnd: false,
                     leaveOnStop: false,
-                }});
-             const trackEmbed = new EmbedBuilder()
+                }
+            });
+
+            const trackEmbed = new EmbedBuilder()
                 .setColor(0x707a7e)
                 .setTitle(`${track.title}`)
                 .setURL(`${track.url}`)
                 .setThumbnail(`${track.thumbnail}`)
                 .setAuthor({ name: `${interaction.user.globalName} played: `, iconURL: `${interaction.user.displayAvatarURL({ dynamic: true, format: 'png', size: 4096 })}` })
                 .setTimestamp();
-                
+
             await interaction.followUp({ embeds: [trackEmbed] });
             await wait(60000);
             await interaction.deleteReply();
-            
+
         } catch (error) {
             return interaction.followUp(`Something went wrong: ${error}`);
         }
