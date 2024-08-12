bebff9f2632ce39699aaefac9fd54727275ebd6e
donnyscript <donnyscript@gmail.com>
2024-08-12 18:46:46 -0400

    Take out comments

--- commands/fun/chat.js
+++ commands/fun/chat.js
@@ -1,9 +1,7 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
 const { spawn } = require('child_process');
 
-// Function to clean up control characters from the output
 function cleanOutput(output) {
-    // Remove ANSI escape codes and trim whitespace
     return output.replace(/[\u001b\u009b][[()#;?]*(?:[0-9]{1,4}(?:;[0-9]{0,4})*)?[0-9A-ORZcf-nqry=><]/g, '').trim();
 }
 
@@ -18,32 +16,26 @@ module.exports = {
     async execute(interaction) {
         const userInput = interaction.options.getString('input');
 
-        // Defer the reply to give time for processing
         await interaction.deferReply();
 
-        // Construct the command to run ollama with the user's input
         const powershellPath = 'C:\\Windows\\System32\\WindowsPowerShell\\v1.0\\powershell.exe';
         const command = 'ollama run dolphin-llama3:8b';
         const childProcess = spawn(powershellPath, ['-Command', command]);
 
         let output = '';
 
-        // Listen for data from stdout
         childProcess.stdout.on('data', (data) => {
             output += data.toString();
         });
 
-        // Listen for the end of the process
         childProcess.on('close', (code) => {
             if (code !== 0) {
                 interaction.editReply('An error occurred while executing the command.');
                 return;
             }
 
-            // Clean the output to remove loading animations and other control characters
             const cleanedOutput = cleanOutput(output);
 
-            // Send the cleaned command output as the reply
             if (cleanedOutput) {
                 interaction.editReply(cleanedOutput);
             } else {
@@ -51,7 +43,6 @@ module.exports = {
             }
         });
 
-        // Send the user's input to the LLM model
         childProcess.stdin.write(`${userInput}\n`);
         childProcess.stdin.end();
     },
