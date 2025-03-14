d96f5dd023f7a3562d40b3251880a0ab35b02c78
DonnyScript <donnyscript@gmail.com>
2025-03-13 20:27:06 -0400

    Update to latest

--- events/ready.js
+++ events/ready.js
@@ -1,6 +1,7 @@
 const { Events } = require('discord.js');
 const { useMainPlayer } = require('discord-player');
 const { YoutubeiExtractor } = require("discord-player-youtubei");
+const { DefaultExtractors } =  require('@discord-player/extractor');
 
 module.exports = {
         name: Events.ClientReady,
@@ -15,7 +16,16 @@ module.exports = {
                         'BridgedExtractor',
                         'AttachmentExtractor',
                 ];
-                player.extractors.loadDefault((extractor) => extractor !== "YouTubeExtractor");
+                await player.extractors.loadMulti(DefaultExtractors);
                 player.extractors.register(YoutubeiExtractor, {});
+                // Player error event listener
+player.events.on('playerError', (queue, error) => {
+        console.error(`Player Error in queue [${queue.guild.name}]:`, error);
+    });
+    
+    // Also consider adding general error event listener:
+    player.events.on('error', (queue, error) => {
+        console.error(`General Error [${queue.guild.name}]:`, error);
+    });
         },
 };
